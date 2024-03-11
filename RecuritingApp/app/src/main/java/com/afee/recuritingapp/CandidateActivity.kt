125f65ca1f0543158b19ba1721f6f9e9a0aa087a
AFEEMAK <afeemaknojia4@gmail.com>
2024-03-10 21:47:21 -0400

    Android Project Part 1 Files Copied

---
+++ RecuritingApp/app/src/main/java/com/afee/recuritingapp/CandidateActivity.kt
@@ -0,0 +1,11 @@
+package com.afee.recuritingapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CandidateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_candidate)
+    }
+}
