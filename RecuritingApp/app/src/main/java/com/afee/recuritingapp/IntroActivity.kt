1cee36d9465c1b3bed8afd0b6bb70b0b8c829920
Vanamuthu <95688625+Vanamuthu@users.noreply.github.com>
2024-03-16 19:55:07 -0400

    Login and SignUp feature added

--- RecuritingApp/app/src/main/java/com/afee/recuritingapp/IntroActivity.kt
+++ RecuritingApp/app/src/main/java/com/afee/recuritingapp/IntroActivity.kt
@@ -15,7 +15,7 @@ class IntroActivity : AppCompatActivity() {
         val introButton: Button = findViewById(R.id.intro_button)
         introButton.setOnClickListener {
 
-            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+            startActivity(Intent(this@IntroActivity, SignInActivity::class.java))
 
             finish()
         }
--- RecuritingApp/app/src/main/java/com/afee/recuritingapp/MainActivity.kt
+++ RecuritingApp/app/src/main/java/com/afee/recuritingapp/MainActivity.kt
@@ -4,11 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnlogout: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnlogout = findViewById(R.id.btnlogout)
+        btnlogout.setOnClickListener{
+            Firebase.auth.signOut()
+
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+
+            Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show()
+
+        }
 
         val viewCandidatesButton: Button = findViewById(R.id.viewCandidatesButton)
         viewCandidatesButton.setOnClickListener {
